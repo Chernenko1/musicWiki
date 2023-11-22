894a53c45efe05c08c1a3575dd4942cc1be1c751
B683fh3rfij3$s <kcheck816@gmail.com>
2023-11-22 19:10:35 +0300

    rewrite navbar

--- src/components/NavBar.tsx
+++ src/components/NavBar.tsx
@@ -1,8 +1,16 @@
 import React from "react";
-import { Navbar, Container, NavLink } from "react-bootstrap";
-import { ADMIN_ROUTE, LOGIN_ROUTE } from "../utils/consts";
+import { Navbar, Container } from "react-bootstrap";
+import { ADMIN_ROUTE, CREATE_ROUTE, LOGIN_ROUTE } from "../utils/consts";
 import { useAppDispatch, useAppSelector } from "../store/hooks";
-import { Box, Button } from "@chakra-ui/react";
+import {
+  Box,
+  Button,
+  Text,
+  Flex,
+  Heading,
+  Spacer,
+  ButtonGroup,
+} from "@chakra-ui/react";
 import { setIsAuth, setUser } from "../store/slices/userSlice";
 import { Link, useNavigate } from "react-router-dom";
 
@@ -18,14 +26,22 @@ export const NavBar = () => {
   };
 
   return (
-    <Navbar bg="dark" data-bs-theme="dark">
-      <Container>
-        <Navbar.Text>
-          <Link to={ADMIN_ROUTE}>AdminPanel</Link>
-        </Navbar.Text>
-        <Navbar.Toggle />
-        <Navbar.Collapse className="justify-content-end">
-          {isAuth ? (
+    <Flex
+      minWidth="max-content"
+      alignItems="center"
+      gap="2"
+      borderBottomWidth={1}
+    >
+      <Box p="2">
+        <Heading size="md">Adminpanel</Heading>
+      </Box>
+      <Spacer />
+      <ButtonGroup gap="2">
+        {isAuth ? (
+          <Box className="d-flex align-items-center px-4">
+            <Link to={CREATE_ROUTE} className="mt-1">
+              <Button className="mr-15">Создать</Button>
+            </Link>
             <Button
               className="mt-3"
               onClick={() => {
@@ -35,13 +51,13 @@ export const NavBar = () => {
             >
               Выйти
             </Button>
-          ) : (
-            <Button>
-              <Link to={LOGIN_ROUTE}>Войти</Link>
-            </Button>
-          )}
-        </Navbar.Collapse>
-      </Container>
-    </Navbar>
+          </Box>
+        ) : (
+          <Button>
+            <Link to={LOGIN_ROUTE}>Войти</Link>
+          </Button>
+        )}
+      </ButtonGroup>
+    </Flex>
   );
 };

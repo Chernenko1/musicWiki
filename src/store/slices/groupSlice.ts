ed4d80113d30791061ed4b92e74e06bf36bc4e5c
B683fh3rfij3$s <kcheck816@gmail.com>
2023-11-24 20:03:41 +0300

    gSlice

--- src/store/slices/groupSlice.ts
+++ src/store/slices/groupSlice.ts
@@ -4,11 +4,35 @@ import { RootState } from "../store";
 interface GroupsState {
   groupsData: [];
   musicStyleData: [];
+  roles: [];
+  cities: [];
+  bandMembers: [];
+  albums: [];
+  songs: [];
+  images: [];
+  videos: [];
+  concerts: [];
+  conscertTickets: [];
+  awards: [];
+  labels: [];
+  preassReleases: [];
 }
 
 const initialState: GroupsState = {
   groupsData: [],
   musicStyleData: [],
+  roles: [],
+  cities: [],
+  bandMembers: [],
+  albums: [],
+  songs: [],
+  images: [],
+  videos: [],
+  concerts: [],
+  conscertTickets: [],
+  awards: [],
+  labels: [],
+  preassReleases: [],
 };
 
 export const groupsSlice = createSlice({
@@ -19,13 +43,63 @@ export const groupsSlice = createSlice({
       state.groupsData = action.payload;
     },
     setMusicStyle(state, action) {
-      console.log(1);
       state.musicStyleData = action.payload;
+    },
+    setRoles(state, action) {
+      state.roles = action.payload;
+    },
+    setCities(state, action) {
+      state.cities = action.payload;
+    },
+    setBandMembers(state, action) {
+      state.bandMembers = action.payload;
+    },
+    setAlbums(state, action) {
+      state.albums = action.payload;
+    },
+    setSongs(state, action) {
+      state.songs = action.payload;
+    },
+    setImages(state, action) {
+      state.images = action.payload;
+    },
+    setVideos(state, action) {
+      state.videos = action.payload;
+    },
+    setConcerts(state, action) {
+      state.concerts = action.payload;
+    },
+    setConcertsTickets(state, action) {
+      state.conscertTickets = action.payload;
+    },
+    setAwards(state, action) {
+      state.awards = action.payload;
+    },
+    setLabels(state, action) {
+      state.labels = action.payload;
+    },
+    setPR(state, action) {
+      state.preassReleases = action.payload;
     },
   },
 });
 
-export const { setGroups, setMusicStyle } = groupsSlice.actions;
+export const {
+  setGroups,
+  setMusicStyle,
+  setAlbums,
+  setAwards,
+  setBandMembers,
+  setCities,
+  setConcerts,
+  setConcertsTickets,
+  setImages,
+  setLabels,
+  setPR,
+  setRoles,
+  setSongs,
+  setVideos,
+} = groupsSlice.actions;
 
 export const selectGroup = (state: RootState) => state.groups;
 
